5c0259687909aa623954595a1db2ff3efc4aa49c
karanbhagat <cv.ryn7@gmail.com>
2017-07-11 14:58:49 -0400

    Little experiment with the app.component to print email alongside the hello angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>
+    Hello {{name}}
+    <p>Email: {{email}}</p>
+  </h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Angular';
+  email = 'xyz@gmail.com'
+}
