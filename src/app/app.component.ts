91ace1d967b8556a5a89de9ced6224fe7a4a2b03
karanbhagat <cv.ryn7@gmail.com>
2017-07-11 16:57:34 -0400

    Added links for home and about page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<router-outlet></router-outlet>`,
+  template: `
+    <ul>
+      <li><a routerLink="/">Home</a> </li>
+      <li><a routerLink="/about">about</a></li>
+    </ul>
+    <hr/>
+    <router-outlet></router-outlet>`,
 })
 export class AppComponent  {
 }
