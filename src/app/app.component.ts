a2307f6076597eb2d821e935dc1c67fc1284cd63
karanbhagat <cv.ryn7@gmail.com>
2017-07-11 15:08:18 -0400

    Made new user component. Registered it in the app.module and replaced the app component template to use user tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>
-    Hello {{name}}
-  </h1>
-    <p><strong>Email:</strong> {{email}}</p>
-  <p><strong>Address:</strong> {{address.street}} {{address.city}} {{address.state}}</p>`,
+  template: `<user></user>`,
 })
 export class AppComponent  {
-  name = 'Angular';
-  email = 'xyz@gmail.com'
-  address = {
-    street: '12 Main st',
-    city: 'Boston',
-    state: 'MA'
-  }
 }
